3003a2db835b5c7f468e1f35fae973d1f26a926c
Thiago Gambarra <tgambarra@mundipagg.com>
2018-03-14 14:42:30 -0300

    :art: primeiro commit

---
+++ src/Camaleao/Camaleao.Api/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Camaleao.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Camaleao.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]ConfRequest confRequest)
+        {
+
+            string output = JsonConvert.SerializeObject(confRequest);
+
+            return Ok();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Camaleao/Camaleao.Repository/MongoContext.cs
@@ -0,0 +1,26 @@
+﻿
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Camaleao.Repository
+{
+    public class MongoContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoContext(Settings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Database);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collection)
+        {
+           
+            return _database.GetCollection<T>(collection);
+        }
+    }
+}
