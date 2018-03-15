7ff30363fa9465b126ab36d1cc695a85886bdc3c
yurisouza <yuri.souza@mundipagg.com>
2018-03-15 17:28:55 -0300

    :art: Validate Contract Mock

---
+++ src/Camaleao/Camaleao.Api/Controllers/MockController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Camaleao.Core.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Camaleao.Api.Controllers
+{
+    [Route("api/mock")]
+    public class MockController : Controller
+    {
+        private readonly MockService _mockService;
+
+        public MockController(MockService mockService)
+        {
+            _mockService = mockService;
+        }
+
+        [HttpPost("{route}")]
+        public async Task<IActionResult> Post(string route, [FromBody]dynamic request)
+        {
+            _mockService.ValidateContract(route, request);
+
+            if(!_mockService.Valid)
+                return new ObjectResult(_mockService.Notifications.FirstOrDefault().Message) { StatusCode = 400 };
+
+
+            return null;
+        }
+
+    }
+}
