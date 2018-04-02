7cbf87c50ef8e307ec257e9eb57388bbef762d90
yurisouza <yuri.souza@mundipagg.com>
2018-04-02 12:15:17 -0300

    :whale: Mudanças para o docker

--- src/Camaleao.Api/Program.cs
+++ src/Camaleao.Api/Program.cs
@@ -19,7 +19,7 @@ namespace Camaleao.Api
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>().UseUrls("http://*:5000")
                 .Build();
     }
 }
