619e1e816b998eb157f893dc5d2aa14072ab6488
Thiago Gambarra <tgambarra@mundipagg.com>
2018-09-11 15:27:37 -0300

    :art: Implementa log

--- src/Camaleao.Core/Services/EngineService.cs
+++ src/Camaleao.Core/Services/EngineService.cs
@@ -5,54 +5,37 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Text;
 
-namespace Camaleao.Core.Services
-{
-    public class EngineService : IEngineService
-    {
+namespace Camaleao.Core.Services {
+    public class EngineService : IEngineService {
         private readonly Engine _engine;
         private readonly IScriptRepository _scriptRepository;
 
-        public EngineService(Engine engine, IScriptRepository scriptRepository)
-        {
+        public EngineService(Engine engine, IScriptRepository scriptRepository) {
             _engine = engine;
             _scriptRepository = scriptRepository;
             LoadPreScript();
         }
 
-        private void LoadPreScript()
-        {
+        private void LoadPreScript() {
             StringBuilder script = new StringBuilder();
             _scriptRepository.GetAll().Result.ForEach(p => script.AppendLine(p.Script));
             _engine.Execute(script.ToString());
         }
 
-        public void LoadRequest(JObject request, string variavel)
-        {
+        public void LoadRequest(JObject request, string variavel) {
             _engine.Execute($"{variavel} = {request}");
         }
 
-        public T Execute<T>(string expression)
-        {
-            try
-            {
-                var result = _engine.Execute(expression).GetCompletionValue().ToString();
-                return (T)Convert.ChangeType(result, Type.GetType(typeof(T).FullName, false, true));
-            }catch(Exception ex)
-            {
+        public T Execute<T>(string expression) {
 
-                throw ex;
-            }
+            var result = _engine.Execute(expression).GetCompletionValue().ToString();
+            return (T)Convert.ChangeType(result, Type.GetType(typeof(T).FullName, false, true));
+
         }
 
-        public string VariableType(string variable)
-        {
-            try
-            {
-                return _engine.Execute($"typeof {variable}").GetCompletionValue().ToString();
-            }catch(Exception ex)
-            {
-                throw ex;
-            }
+        public string VariableType(string variable) {
+
+            return _engine.Execute($"typeof {variable}").GetCompletionValue().ToString();
         }
     }
 }
